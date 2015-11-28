b277c89bb84c47e670961a68f4c5b9dd5efbd44d
Thomas Asadurian <Tasadurian@gmail.com>
2015-11-28 13:41:51 -0800

    refactoring to focus on the front end

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var halo = require('../halo.js');
 
 /* GET home page. */
 
 router.get('/', function(req, res) {
-  var matches = halo.matches;
-  console.log(matches);
   res.render('index', {
     title: 'Express',
-    matches: matches
   });
 });
 
