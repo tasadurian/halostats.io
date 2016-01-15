6ba76bac1b0d7dedcdc37988b1258a57b23c0de8
Thomas Asadurian <Tasadurian@gmail.com>
2016-01-14 20:03:13 -0800

    initial state is set

--- public/js/components/controller.jsx
+++ public/js/components/controller.jsx
@@ -4,17 +4,17 @@ var ReactDOM = require('react-dom');
 var KillsDeaths = require('./kills-deaths.jsx');
 var WinsLoss = require('./wins-loss.jsx');
 
-var componentArray = ['<KillsDeaths />', '<WinsLoss />'];
+var componentArray = [<KillsDeaths />, <WinsLoss />];
 
 var Controller = React.createClass({
   getInitialState: function() {
     return {
-      component: componentArray[0]
+      component: componentArray[1]
     };
   },
   render: function() {
     return (
-      <div><WinsLoss /></div>
+      <div>{this.state.component}</div>
     );
   }
 });
